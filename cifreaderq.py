d19da606160292095af0a5dd5fcc5f0c1df2e2e4
Evgenyi <evgenyi.logachev@gmail.com>
2023-06-06 17:13:29 +0300

    формат

--- cifreaderq.py
+++ cifreaderq.py
@@ -5,6 +5,7 @@ import ctypes
 
 def cifreader(f):
     if os.path.splitext(f)[1]==".cif":
-        cluster_count=np.fromfile(f,count=9,offset=12,dtype=ctypes.c_uint)
+        cluster_count=np.fromfile(f,count=9,offset=12,dtype=ctypes.c_ulong)#ctypes.c_uint
+        print(cluster_count)
         print()
-        unknow=np.fromfile(f,count=12,offset=-1,dtype=ctypes.c_uint)
+        unknow=np.fromfile(f,count=12,offset=-1,dtype=ctypes.c_uint)#dtype,format?#['base':[cluster_count*intensitivy]] or [[cluster_count*intensitivy],...,[cluster_count*intensitivy]]
