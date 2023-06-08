26490460914623df25938d79570da22960a4781c
Evgenyi <evgenyi.logachev@gmail.com>
2023-06-09 01:30:59 +0300

    080623

--- cifreaderq.py
+++ cifreaderq.py
@@ -5,13 +5,17 @@ import ctypes
 
 def cifreader(f):
     if os.path.splitext(f)[1]==".cif":
-        cluster_count=np.fromfile(f,count=-1,offset=9,dtype=ctypes.c_ulong)#ctypes.c_uint
+        cluster_count=np.fromfile(f,count=4,offset=9,dtype=ctypes.c_ulong)#ctypes.c_uint
         print(cluster_count)
+        print(type(cluster_count))
         print()
-        
+        intensitivity=np.fromfile(f,count=-1,offset=12,dtype=ctypes.c_ulong)#ctypes.c_uint
+        print(intensitivity)
+        print(type(intensitivity))
+        print()
         
         
 
 
 if __name__ == '__main__':
-    cifreader("C:/Users/Евгений/Downloads/s_1_1101.cif")
+    cifreader("C:/Users/evgen/Downloads/DNATOOOLS/files/s_1_1101.cif")
